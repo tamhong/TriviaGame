99f90f1fb4b122b27f9a836ca086c9d943b692ab
Tam Hong <tamhong.sd@gmail.com>
2018-06-21 17:11:32 -0700

    finished unstyled

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -51,7 +51,7 @@ var myQuestions = [
             b: "answer",
             c: "answer",
         },
-        correctAnswer: "d"
+        correctAnswer: "c"
     },
     {
         question: "q?",
@@ -60,7 +60,7 @@ var myQuestions = [
             b: "answer",
             c: "answer",
         },
-        correctAnswer: "d"
+        correctAnswer: "a"
     },
     {
         question: "q?",
@@ -73,12 +73,32 @@ var myQuestions = [
     }
 ];
 
-var selected = [];
+var right = 0;
+var wrong = 0;
+var unanswered= 0;
 
 
 
-$("#start").click(function showQuestions() {
+$("#start").click(function timedQuiz() {
     $("#startBtn").empty();
+
+    $("#timer").text(60);
+
+    var n = 60;
+    var tm = setInterval(countDown,1000);
+
+    function countDown(){
+        n--;
+        $("#timer").text(n);
+        if(n == 0){
+            clearInterval(tm);
+        }
+        console.log(n);
+
+        $("#done").click(function stopTimer () {
+            clearInterval(tm);
+        })
+    }
 
     var answers;
 
@@ -106,24 +126,89 @@ $("#start").click(function showQuestions() {
 
 $(document).on("click","#done",function(){
 
-        var selValue1= $('input[name=question0]:checked').val();
-        var selValue2= $('input[name=question1]:checked').val();
-        var selValue3= $('input[name=question2]:checked').val();
-        var selValue4= $('input[name=question3]:checked').val();
-        var selValue5= $('input[name=question4]:checked').val();
-        var selValue6= $('input[name=question5]:checked').val();
-        var selValue7= $('input[name=question6]:checked').val();
-        var selValue8= $('input[name=question7]:checked').val();
+    $("#timer").empty()
+    $("#change-content").hide()
 
-    console.log(selValue1, selValue2, selValue3, selValue4, selValue5, selValue6, selValue7, selValue8)
+        var selValue0= $('input[name=question0]:checked').val();
+        if (selValue0 === myQuestions[0].correctAnswer) {
+            right++
+        } else if (selValue0 === undefined) {
+            unanswered++
+        } else {
+            wrong++
+        }
+        var selValue1= $('input[name=question1]:checked').val();
+        if (selValue1 === myQuestions[1].correctAnswer) {
+            right++
+        } else if (selValue1 === undefined) {
+            unanswered++
+        } else {
+            wrong++
+        }
+        var selValue2= $('input[name=question2]:checked').val();
+        if (selValue2 === myQuestions[2].correctAnswer) {
+            right++
+        } else if (selValue2 === undefined) {
+            unanswered++
+        } else {
+            wrong++
+        }
+        var selValue3= $('input[name=question3]:checked').val();
+        if (selValue3 === myQuestions[3].correctAnswer) {
+            right++
+        } else if (selValue3 === undefined) {
+            unanswered++
+        } else {
+            wrong++
+        }
+        var selValue4= $('input[name=question4]:checked').val();
+        if (selValue4 === myQuestions[4].correctAnswer) {
+            right++
+        } else if (selValue4 === undefined) {
+            unanswered++
+        } else {
+            wrong++
+        }
+        var selValue5= $('input[name=question5]:checked').val();
+        if (selValue5 === myQuestions[5].correctAnswer) {
+            right++
+        } else if (selValue5 === undefined) {
+            unanswered++
+        } else {
+            wrong++
+        }
+        var selValue6= $('input[name=question6]:checked').val();
+        if (selValue6 === myQuestions[6].correctAnswer) {
+            right++
+        } else if (selValue6 === undefined) {
+            unanswered++
+        } else {
+            wrong++
+        }
+        var selValue7= $('input[name=question7]:checked').val();
+        if (selValue7 === myQuestions[7].correctAnswer) {
+            right++
+        } else if (selValue7 === undefined) {
+            unanswered++
+        } else {
+            wrong++
+        }
 
-    for(let [i, question] of myQuestions.entries()){      
-        guess = $('input[name=question'+ i+']:checked').val();
-        console.log(guess)
-        console.log(question.correctAnswer)
-    }
+    console.log(selValue0, selValue1, selValue2, selValue3, selValue4, selValue5, selValue6, selValue7)
+    console.log(right)
+    console.log(wrong)
+    console.log(unanswered)
+
+    $("#results").append('Correct: ' + right)
+    $("#results").append('<br> Wrong: ' + wrong)
+    $("#results").append('<br> Unanswered: ' + unanswered)
 
 });
+
+
+//console.log(right)
+//console.log(wrong)
+
 
 
 
